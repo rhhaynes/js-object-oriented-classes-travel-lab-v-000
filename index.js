95ce99e3853828cee6bcbbd361b3555cb06d9130
Randall Harris Haynes <harrishaynes@gmail.com>
2018-05-23 14:00:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ class Route {
     const hDis = Math.abs( eastWest.indexOf(this.beginningLocation.horizontal) - eastWest.indexOf(this.endingLocation.horizontal) );
     return vDis + hDis;
   }
-  estimatedTime(){
-    return 
+  estimatedTime(peakHours){
+    return peakHours
   }
 }
