c5f545e7f6d3978542b376717fdd0e5ff69bf908
Randall Harris Haynes <harrishaynes@gmail.com>
2018-05-23 13:52:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ class Route {
   }
   blocksTravelled(){
     const eastWest = ['1st Avenue', '2nd Avenue', '3rd Avenue', 'Lexington Avenue', 'Park', 'Madison Avenue', '5th Avenue'];
-    
+    const vDis = this.beginningLocation.vertical
   }
   estimatedTime(){
     bla
